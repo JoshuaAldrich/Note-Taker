cc77a66fc2b4db11a4cd11cebb492828deb5df72
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-10-08 11:27:57 -0400

    Fixed some errors with api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,16 @@
-router.get("/api/notes", (req, res) => {
-  const data = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+
+router.get("/notes", (req, res) => {
+  const data = fs.readFileSync(path.join(__dirname, "../../db/db.json"));
   const notes = JSON.parse(data);
   res.json(notes);
 });
 
-router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
   const title = req.body.title;
   const text = req.body.text;
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,39 +3,19 @@ const PORT = process.env.PORT || 3001;
 const path = require("path");
 const fs = require("fs");
 const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require(".routes/htmlRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 
 // make files in public directory available
 app.use(express.static("public"));
 
-//routes directory
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
-
 //parse JSON data
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
-
-// app.get("/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/notes.html"));
-// });
-
-// app.get("/api/notes", (req, res) => {
-//   const data = fs.readFileSync(path.join(__dirname, "./db/db.json"));
-//   const notes = JSON.parse(data);
-//   res.json(notes);
-// });
-
-// app.post("/api/notes", (req, res) => {
-//   const title = req.body.title;
-//   const text = req.body.text;
-// });
+//routes directory
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log("API server now on port ${PORT}.");
